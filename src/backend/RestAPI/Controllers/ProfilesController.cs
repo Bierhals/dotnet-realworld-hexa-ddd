8e9563914f1b8668433db5cf7adcf35a0f550089
Sven-Uwe Bierhals <sven-uwe@bierhals-online.de>
2024-01-03 17:48:17 +0100

    weitere profile methoden

--- src/backend/RestAPI/Controllers/ProfilesController.cs
+++ src/backend/RestAPI/Controllers/ProfilesController.cs
@@ -43,4 +43,64 @@ public class ProfilesController : ControllerBase
                 }
             });
     }
+
+    /// <summary>
+    /// Follow a user by username
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://realworld-docs.netlify.app/docs/specs/backend-specs/endpoints/#follow-user">Conduit Spec for follow user endpoint</a>
+    /// </remarks>
+    /// <param name="username">Username of the profile you want to follow</param>
+    /// <returns></returns>
+    /// <response code="200">Returns the followed user's detailed profile information</response>
+    /// <response code="401">Unauthorized</response>
+    /// <response code="422">User not found</response>
+    [HttpPost("{username}/follow")]
+    [ProducesResponseType<ProfileResponse>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+    public IActionResult FollowUserByUsername(string username)
+    {
+        return Ok(
+            new ProfileResponse
+            {
+                Profile = new Profile
+                {
+                    Username = "name",
+                    Bio = "Test Bio",
+                    Image = "Test",
+                    Following = false
+                }
+            });
+    }
+
+    /// <summary>
+    /// Unfollow a user
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://realworld-docs.netlify.app/docs/specs/backend-specs/endpoints#unfollow-user">Conduit Spec for unfollow user endpoint</a>
+    /// </remarks>
+    /// <param name="username">Username of the profile you want to unfollow</param>
+    /// <returns></returns>
+    /// <response code="200">Returns the unfollowed user's detailed profile information</response>
+    /// <response code="401">Unauthorized</response>
+    /// <response code="422">User not found</response>
+    [HttpDelete("{username}/follow")]
+    [ProducesResponseType<ProfileResponse>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+    public IActionResult UnfollowUserByUsername(string username)
+    {
+        return Ok(
+            new ProfileResponse
+            {
+                Profile = new Profile
+                {
+                    Username = "name",
+                    Bio = "Test Bio",
+                    Image = "Test",
+                    Following = false
+                }
+            });
+    }
 }
