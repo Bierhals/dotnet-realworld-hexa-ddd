6b781fd4e3246adb56527553cb5db367afba02dd
Sven-Uwe Bierhals <sven-uwe@bierhals-online.de>
2024-01-19 21:08:44 +0100

    Initial hasher

--- src/backend/Domain/User/IPasswordHasher.cs
+++ src/backend/Domain/User/IPasswordHasher.cs
@@ -3,5 +3,5 @@ namespace Conduit.Domain;
 public interface IPasswordHasher
 {
     string HashPassword(string clearTextPassword);
-    string VerifyPassword(string clearTextPassword, string hashedPassword);
+    bool VerifyPassword(string clearTextPassword, string hashedPassword);
 }
---
+++ src/backend/Persistence/Users/PasswordHasherVersion.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+
+namespace Conduit.Persistence;
+
+record PasswordHasherVersion
+{
+    public required HashAlgorithmName Algorithm { get; init; }
+    public required int SaltSize { get; init; }
+    public required int KeySize { get; init; }
+    public required int Iterations { get; init; }
+}
