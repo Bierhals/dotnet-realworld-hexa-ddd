15cb0379e24335223f203e9719bfea60746f671f
Sven-Uwe Bierhals <sven-uwe@bierhals-online.de>
2026-05-19 18:37:14 +0200

    moved backend frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        'target': 'https://localhost:5001',
+        'target': 'http://localhost:5000',
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '') 
       },
